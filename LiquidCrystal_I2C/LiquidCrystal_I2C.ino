467f03bd61301ddce1c6a3e6344d36816e99e973
Bruno de Souza <brunnodesouzabezerra@gmail.com>
2020-03-04 11:54:12 -0300

    Update lcd.begin(), lcd.home() and lcd.print();

--- LiquidCrystal_I2C/LiquidCrystal_I2C.ino
+++ LiquidCrystal_I2C/LiquidCrystal_I2C.ino
@@ -5,13 +5,13 @@
 LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
 
 void setup() {
-  lcd.begin (16,2);
+  lcd.begin(16,2);
   lcd.setBacklight(HIGH);
 }
 
 void loop() {
-  lcd.home ();
+  lcd.home();
   lcd.print("Hello, World!");
   lcd.setCursor(0, 1);
-  lcd.print ("NewLiquidCrystal");
+  lcd.print("NewLiquidCrystal");
 }
