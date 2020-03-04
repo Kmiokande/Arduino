550e19a1070b49081e54f468fac70abbb02d53e3
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-03 23:43:08 -0300

    Upload Liquid Crystal I2C

---
+++ LiquidCrystal_I2C/LiquidCrystal_I2C.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Inicializa o display no endereço 0x27
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+
+void setup() {
+  lcd.begin (16,2);
+  lcd.setBacklight(HIGH);
+}
+
+void loop() {
+  lcd.home ();
+  lcd.print("Hello, World!");
+  lcd.setCursor(0, 1);
+  lcd.print ("NewLiquidCrystal");
+}
