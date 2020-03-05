efb5d6cccab7480b98c28065420de1860ffda93e
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-03-04 21:05:31 -0300

    Upload 4-Digit 7 Segment LED Display project

---
+++ 4_Digit_7_Segment/4_Digit_7_Segment.ino
@@ -0,0 +1,21 @@
+#include "SevSeg.h"
+
+SevSeg sevseg;  //Instantiate a seven segment object
+
+void setup(){
+  byte numDigits = 4;
+  byte digitPins[] = {10, 11, 12, 13};
+  byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
+  bool resistorsOnSegments = true;  // 'false' means resistors are on digit pins
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_CATHODE;
+  
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
+  updateWithDelaysIn);
+  sevseg.setBrightness(90);
+}
+
+void loop(){
+  sevseg.setNumber(3141, 3);
+  sevseg.refreshDisplay();
+}
---
+++ 4_Digit_7_Segment/README.md
@@ -0,0 +1,7 @@
+# 4-Digit 7 Segment LED Display
+
+![4-Digit 7 Segment LED Display Fritzing Project](4_Digit_7_Segment.png)
+
+1. Para controlar o Display de 4 dígitos, utilize a biblioteca SevSeg:
+
+- [Repositório SevSeg](https://github.com/DeanIsMe/SevSeg)
