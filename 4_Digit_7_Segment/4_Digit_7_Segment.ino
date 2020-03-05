7ba137af3ace892b0a3a9c49d75cf3029aac3594
Bruno de Souza <brunnodesouzabezerra@gmail.com>
2020-03-04 21:07:50 -0300

    Add spaces

--- 4_Digit_7_Segment/4_Digit_7_Segment.ino
+++ 4_Digit_7_Segment/4_Digit_7_Segment.ino
@@ -2,7 +2,7 @@
 
 SevSeg sevseg;  //Instantiate a seven segment object
 
-void setup(){
+void setup() {
   byte numDigits = 4;
   byte digitPins[] = {10, 11, 12, 13};
   byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
@@ -15,7 +15,7 @@ void setup(){
   sevseg.setBrightness(90);
 }
 
-void loop(){
+void loop() {
   sevseg.setNumber(3141, 3);
   sevseg.refreshDisplay();
 }
